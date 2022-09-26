6bc2c7151eff8e5d599c8b1d925ded05676a40d1
AbdulRaffiNaufal <113502265+AbdulRaffiNaufal@users.noreply.github.com>
2022-09-26 22:32:11 +0700

    Add files via upload

---
+++ latihan-service/latihan-service/src/test/java/com/naufal/latihan/service/LatihanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naufal.latihan.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
